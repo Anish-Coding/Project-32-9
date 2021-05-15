41bc6fe23d1777987196ca73fdcb435eac2a61c2
Anish-Coding <73768987+Anish-Coding@users.noreply.github.com>
2021-05-15 17:55:20 +0530

    Add files via upload

---
+++ js/BaseClass.js
@@ -0,0 +1,40 @@
+class BaseClass{
+    constructor(x, y, width, height, angle) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0,
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("images/download.png");
+        World.add(world, this.body);
+      }
+
+      display(){
+        console.log("this.x =", this.x, " :: this.y = " + this.y);
+        console.log("this.body.position.x =", this.body.position.x, " :: this.body.position.y = " + this.body.position.y);
+
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
+
+      setWidthandHeight(width, height) {
+        this.width = width;
+        this.height = height;
+      }
+
+      changeDbPosition(x, y){
+        this.body.position.x = x;
+        this.body.position.y = y;
+      }
+
+}
